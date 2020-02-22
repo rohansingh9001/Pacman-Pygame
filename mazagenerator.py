be8e61e4d5673c61a2adadf2ae892e0f1a8fac7b
Rohan Singh <rohansingh9001>
2020-02-23 01:33:49 +0530

    Added images and added maze generation algorithm

---
+++ mazagenerator.py
@@ -0,0 +1,43 @@
+from PIL import Image
+
+# put imgname equal to the image to be solved
+
+imgname = "Maze.png"
+
+
+def array(imgname):
+
+    image = Image.open(imgname)
+
+    width, height = image.size
+
+    maze = []
+
+    for i in range(width):
+
+        row = []
+
+        for j in range(height):
+
+            if image.getpixel((j, i))[0] == 255:
+
+                row.append(0)
+
+            else:
+
+                row.append(1)
+
+        maze.append(row)
+
+    return maze
+
+
+    # node making algorithm
+    # key --
+    # 0 = wall
+    # 1 = connecting nodes
+    # 2 = nodes
+    # 3 = start node
+    # 4 = end node
+if __name__ == "__main__":
+    array(imgname)
--- pacman.py
+++ pacman.py
@@ -1,29 +1,40 @@
 import pygame
 import time
+from mazagenerator import array
 
 # initialise pygame
 pygame.init()
-tile_size = 8
-sprite_size = 16
+tile_size = 16  # 1 tile is 16x16 px
+sprite_size = 16  # 1 sprite is 16x16 tile i.e. (16x16)x(16x16) px
 
 # The Map
 # Key
 # Wall - 1
 # Path - 0
 
-map_x = 8
-map_y = 8
-map = [
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1, 0, 0, 1],
-    [1, 0, 0, 1, 1, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1],
+# The map img name
+map_img = 'Maze.png'
 
-]
+map = array(map_img)
+map_x = len(map[0])
+map_y = len(map)
+
+
+# Create a tuple addition function which returns the direction.
+
+
+def get_block(current, direction):
+    curr_x, curr_y = current
+    dir_x, dir_y = direction
+
+    return (curr_x + dir_x, curr_y + dir_y)
+
+
+# Create a function to convert the coordinates to px
+def coor_to_px(coordinates):
+    x, y = coordinates
+    return (x * tile_size, y * tile_size)
+
 
 # Title and Icon
 pygame.display.set_caption("PacMan")
@@ -31,10 +42,25 @@ icon = pygame.image.load("pacman_open.png")
 pygame.display.set_icon(icon)
 
 # create the screen
-screen = pygame.display.set_mode((tile_size*8, tile_size*8))
+screen = pygame.display.set_mode((tile_size * map_x, tile_size * map_y))
 
 # Load images
 wall_img = pygame.image.load('wall.png')
+wall_1111 = pygame.image.load('wall_1111.png')
+wall_1110 = pygame.image.load('wall_1110.png')
+wall_1101 = pygame.image.load('wall_1101.png')
+wall_1011 = pygame.image.load('wall_1011.png')
+wall_0111 = pygame.image.load('wall_0111.png')
+wall_1100 = pygame.image.load('wall_1100.png')
+wall_1001 = pygame.image.load('wall_1001.png')
+wall_0011 = pygame.image.load('wall_0011.png')
+wall_0110 = pygame.image.load('wall_0110.png')
+wall_1000 = pygame.image.load('wall_1000.png')
+wall_0001 = pygame.image.load('wall_0001.png')
+wall_0010 = pygame.image.load('wall_0010.png')
+wall_0100 = pygame.image.load('wall_0100.png')
+wall_1010 = pygame.image.load('wall_1010.png')
+wall_0101 = pygame.image.load('wall_0101.png')
 path_img = pygame.image.load('path.png')
 player_img = pygame.image.load('pacman_open.png')
 # Player's initial position in tiles
@@ -42,20 +68,56 @@ playerX = 0
 playerY = 0
 
 
-def player(x, y):
-    screen.blit(player_img, (x*tile_size, y*tile_size))
+def create_map():
+    # Code 1111: up, right, down, left
+    for x in range(1, map_x-1):
+        for y in range(1, map_y-1):
 
+            i = map[y][x]
+            u = map[y-1][x]
+            d = map[y+1][x]
+            l = map[y][x-1]
+            r = map[y][x+1]
+            if i == 0:
+                screen.blit(path_img, coor_to_px((x, y)))
+            if i == 1:
+                if u == 1 and d == 1 and r == 1 and l == 1:
+                    screen.blit(wall_img, coor_to_px((x, y)))
 
-def create_map(x, y, i):
-    if i == 1:
-        screen.blit(wall_img, (x*tile_size, y*tile_size))
-    if i == 0:
-        screen.blit(path_img, (x*tile_size, y*tile_size))
+                if u == 0 and d == 1 and r == 1 and l == 1:
+                    screen.blit(wall_1000, coor_to_px((x, y)))
+                if u == 1 and d == 0 and r == 1 and l == 1:
+                    screen.blit(wall_0010, coor_to_px((x, y)))
+                if u == 1 and d == 1 and r == 0 and l == 1:
+                    screen.blit(wall_0100, coor_to_px((x, y)))
+                if u == 1 and d == 1 and r == 1 and l == 0:
+                    screen.blit(wall_0001, coor_to_px((x, y)))
+
+                if u == 0 and d == 0 and r == 1 and l == 1:
+                    screen.blit(wall_1010, coor_to_px((x, y)))
+                if u == 1 and d == 0 and r == 0 and l == 1:
+                    screen.blit(wall_0110, coor_to_px((x, y)))
+                if u == 1 and d == 1 and r == 0 and l == 0:
+                    screen.blit(wall_0101, coor_to_px((x, y)))
+                if u == 0 and d == 1 and r == 1 and l == 0:
+                    screen.blit(wall_1001, coor_to_px((x, y)))
+                if u == 1 and d == 0 and r == 1 and l == 0:
+                    screen.blit(wall_0011, coor_to_px((x, y)))
+                if u == 0 and d == 1 and r == 0 and l == 1:
+                    screen.blit(wall_1100, coor_to_px((x, y)))
+
+                if u == 1 and d == 0 and r == 0 and l == 0:
+                    screen.blit(wall_0111, coor_to_px((x, y)))
+                if u == 0 and d == 1 and r == 0 and l == 0:
+                    screen.blit(wall_1101, coor_to_px((x, y)))
+                if u == 0 and d == 0 and r == 1 and l == 0:
+                    screen.blit(wall_1011, coor_to_px((x, y)))
+                if u == 0 and d == 0 and r == 0 and l == 1:
+                    screen.blit(wall_1110, coor_to_px((x, y)))
+
 
 # Create the
 # The wall class
-
-
 class Wall():
     def __init__(self, x, y):
         self.x = x
@@ -70,19 +132,34 @@ class Path():
         self.coordinate = (x, y)
 
 
+class Pacman():
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.coordinate = (x, y)
+        self.prev = None
+        self.direction = (1, 0)
+        self.next = get_block(self.coordinate, self.direction)
+
+    # The player placement function
+
+    def place(self):
+        screen.blit(player_img, coor_to_px(self.coordinate))
+
+
 # Game Loop
 running = True
 while running:
     # RGB = Red, Green, Blue
     screen.fill((0, 0, 0))
 
+    pacman = Pacman(2, 2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    for i in range(map_x):
-        for j in range(map_y):
-            create_map(i, j, map[j][i])
-    player(1, 1)
+    create_map()
+    pacman.place()
     pygame.display.update()
     time.sleep(0.1)
